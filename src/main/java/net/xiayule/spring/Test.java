6bf72ed0898f37c9568a23db520b35adeac5310e
Hehe Tan <xiayule148@gmail.com>
2014-12-16 15:47:35 +0800

    遍历 root

--- src/main/java/net/xiayule/spring/Test.java
+++ src/main/java/net/xiayule/spring/Test.java
@@ -3,24 +3,38 @@ package net.xiayule.spring;
 
 import net.xiayule.spring.core.io.ClassPathResource;
 import org.junit.Assert;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
-import java.net.URI;
-import java.net.URL;
+import java.lang.annotation.ElementType;
 
 /**
  * Created by tan on 14-12-15.
  */
 public class Test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         ClassPathResource classPathResource = new ClassPathResource("spring-config.xml");
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(classPathResource.getInputStream()));
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(classPathResource.getInputStream());
 
-        String t;
-        while ((t = reader.readLine()) != null) {
-            System.out.println(t);
+        Element root = doc.getDocumentElement();
+
+        NodeList nl = root.getChildNodes();
+        for (int i=0; i<nl.getLength(); i++) {
+            Node node = nl.item(i);
+            if (node instanceof Element) {
+                Element ele = (Element) node;
+                System.out.println(ele);
+            }
         }
-
     }
 }
